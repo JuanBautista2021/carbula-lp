5f8f91b55aa124610eccb98d91f6c9f6f5cf31a7
Desarrollo <jbmg21@protonmail.com>
2022-01-17 17:13:43 -0300

    Changed class to className

--- pages/index.js
+++ pages/index.js
@@ -12,16 +12,16 @@ export default function Home() {
       </Head>
 
       <main className={styles.main + " bg-primary bg-gradient"}>
-      <div class="row  justify-content-center align-items-center">
-        <div class="col-md-6">
-          <img src="../images/logo.svg" class="img-fluid" alt="Logo"></img>
+      <div className="row  justify-content-center align-items-center">
+        <div className="col-md-6">
+          <img src="../images/logo.svg" className="img-fluid" alt="Logo"></img>
         </div>
-        <div class="col-md-6">
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item bg-transparent"><a href="https://www.carbula.ar/" class="btn btn-outline-light border-0" role="button"><img src="../images/banderas/flgAR.svg" className={styles.icoflg} alt="Bandera AR"></img>Argentina</a></li>
-            <li class="list-group-item bg-transparent"><a href="https://www.carbula.cl/" class="btn btn-outline-light border-0" role="button"><img src="../images/banderas/flgCL.svg" className={styles.icoflg} alt="Bandera CL"></img>Chile</a></li>
-            <li class="list-group-item bg-transparent"><a href="https://www.carbula.mx/" class="btn btn-outline-light border-0" role="button"><img src="../images/banderas/flgMX.svg" className={styles.icoflg} alt="Bandera MX"></img>Mexico</a></li>
-            <li class="list-group-item bg-transparent"><a href="https://www.carbula.uy/" class="btn btn-outline-light border-0" role="button"><img src="../images/banderas/flgUY.svg" className={styles.icoflg} alt="Bandera UY"></img>Uruguay</a></li>
+        <div className="col-md-6">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item bg-transparent"><a href="https://www.carbula.ar/" className="btn btn-outline-light border-0" role="button"><img src="../images/banderas/flgAR.svg" className={styles.icoflg} alt="Bandera AR"></img>Argentina</a></li>
+            <li className="list-group-item bg-transparent"><a href="https://www.carbula.cl/" className="btn btn-outline-light border-0" role="button"><img src="../images/banderas/flgCL.svg" className={styles.icoflg} alt="Bandera CL"></img>Chile</a></li>
+            <li className="list-group-item bg-transparent"><a href="https://www.carbula.mx/" className="btn btn-outline-light border-0" role="button"><img src="../images/banderas/flgMX.svg" className={styles.icoflg} alt="Bandera MX"></img>Mexico</a></li>
+            <li className="list-group-item bg-transparent"><a href="https://www.carbula.uy/" className="btn btn-outline-light border-0" role="button"><img src="../images/banderas/flgUY.svg" className={styles.icoflg} alt="Bandera UY"></img>Uruguay</a></li>
           </ul>
         </div>
       </div>
